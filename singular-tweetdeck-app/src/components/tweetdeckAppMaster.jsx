d08777b607ec4ff21125b21be53fb6aad7adc511
tmSingular <thomas@singular.live>
2019-07-29 23:02:31 +0200

    Tweetdeck - adding bootstrap tabs

--- singular-tweetdeck-app/src/components/tweetdeckAppMaster.jsx
+++ singular-tweetdeck-app/src/components/tweetdeckAppMaster.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import AppTweetdeckSetup from "./tweetdeckAppSetup";
 import AppTweetdeckPlayout from "./tweetdeckAppPlayout";
+import { Tabs, Tab } from "react-bootstrap";
 
 class AppTweetdeckMaster extends Component {
   state = {};
+
   render() {
     console.log("AppTweetdeckMaster - called");
     return (
@@ -11,35 +13,24 @@ class AppTweetdeckMaster extends Component {
         <nav className="navbar navbar-light bg-light">
           <span className="navbar-brand mb-0 h1">Tweetdeck App</span>
         </nav>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            className="navbar-nav"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-item nav-link active" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
-              <a className="nav-item nav-link" href="#">
-                Features
-              </a>
-            </div>
-          </div>
-        </nav>
-        <AppTweetdeckSetup />
-        <AppTweetdeckPlayout />
+        <Tabs
+          className="m-2"
+          id="controlled-tab-example"
+          activeKey={this.state.key}
+          onSelect={k => this.setState({ key: k })}
+        >
+          <Tab eventKey="playout" title="Playout">
+            <h4>Welcome to playout ......</h4>
+            <AppTweetdeckPlayout />
+          </Tab>
+          <Tab eventKey="setup" title="Setup">
+            <h4>Welcome to setup...</h4>
+            <AppTweetdeckSetup />
+          </Tab>
+          <Tab eventKey="debug" title="Debug">
+            <h4>Welcome to debug...</h4>
+          </Tab>
+        </Tabs>
       </React.Fragment>
     );
   }
--- singular-tweetdeck-app/src/components/tweetdeckAppPlayout.jsx
+++ singular-tweetdeck-app/src/components/tweetdeckAppPlayout.jsx
@@ -2,6 +2,28 @@ import React, { Component } from "react";
 
 class AppTweetdeckPlayout extends Component {
   state = {};
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.twitter.com/1.1/collections/list.json?screen_name=singular4live&count=1")
+      .then(Response => Response.json())
+      .then(res => {
+        console.log(res);
+        this.setState({
+          items: res
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
     console.log("AppTweetdeckPlayout - called");
     return (
